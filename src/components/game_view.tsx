d694440d9af349eda54a29e6f83f5415503fed93
yuval kleiman <yuva.kleiman@me.com>
2022-07-15 23:33:48 +0300

    add lots of stuff
    Co-authored-by: TomerNorman <HitBox38@users.noreply.github.com>
    Co-authored-by: Kfir Nevo <realKfiros@users.noreply.github.com>

---
+++ src/components/game_view.tsx
@@ -0,0 +1,48 @@
+import {useContext, useEffect} from 'react';
+import {observer} from 'mobx-react-lite';
+import {GameContext} from '../stores/game_store';
+import {css} from '@emotion/react';
+
+const styleGameView = css`
+    
+    .player {
+        
+    }
+`;
+const GameView = observer(() =>
+{
+    const store = useContext(GameContext);
+
+    useEffect(() =>
+    {
+        gameLoop();
+    }, []);
+
+    const gameLoop = () =>
+    {
+        gameLoop();
+    };
+
+    return <div css={styleGameView}>
+        <div className='tableView'>
+            <div className='player'></div>
+            <div className='gametable'></div>
+            <div className='diceView'></div>
+            
+        </div>
+        <div className='mapView'>
+            <div className='characters'>
+                <div className='player'></div>
+                <div className='NPCs'>
+                    
+                </div>
+            </div>
+            <div className='worldObjects'>
+                <div className='gametable'></div>
+            </div>
+        </div>
+        
+    </div>;
+});
+
+export default GameView;
