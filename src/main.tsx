3616c50b7514061d6260f08fca670d8a97227c3c
Tomer Norman <spidernor38@gmail.com>
2022-07-16 22:49:25 +0300

    disabled the context menu

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import {AppContext, Store} from "./store";
-import {configure} from 'mobx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { AppContext, Store } from "./store";
+import { configure } from "mobx";
 
 configure({
-	useProxies: 'never'
+  useProxies: "never",
 });
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	// <React.StrictMode>
-		<AppContext.Provider value={new Store() as any}>
-			<App/>
-		</AppContext.Provider>
-	// </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  // <React.StrictMode>
+  <div onContextMenu={(e) => e.preventDefault()}>
+    <AppContext.Provider value={new Store() as any}>
+      <App />
+    </AppContext.Provider>
+  </div>
+  // </React.StrictMode>
 );
