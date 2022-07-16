06f0929aa61c2c9c8fa2b878337d805ee70c4d41
Tomer Norman <spidernor38@gmail.com>
2022-07-16 11:37:55 +0300

    fixed clip path by removing it

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import {useContext, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import {observer} from "mobx-react-lite";
-import MainMenu from './components/Menus/MainMenu';
-import {AppContext, StoreProps} from "./store";
-import {GameContext, GameStore} from './stores/game_store';
-import GameView from './components/game_view';
+import { useContext, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { observer } from "mobx-react-lite";
+import MainMenu from "./components/menus/MainMenu";
+import { AppContext, StoreProps } from "./store";
+import { GameContext, GameStore } from "./stores/game_store";
+import GameView from "./components/game_view";
 
-const App = observer(() =>
-{
-	const store: any = useContext(AppContext);
-	const {count}: any = store;
+const App = observer(() => {
+  const store: any = useContext(AppContext);
+  const { count }: any = store;
 
-	return <div className="App">
-		<div className="GameCanvas" style={{height:"100vh",width:"100vw"}}>
-			<div className="BackgroundTexture"></div>
-			<div className="ForegroundElements">
-				<MainMenu></MainMenu>
-				<GameContext.Provider value={new GameStore()}>
-				<GameView></GameView>
-				</GameContext.Provider>
-			</div>
-		</div>
-	</div>
+  return (
+    <div className="App">
+      <div className="GameCanvas" style={{ height: "100vh", width: "100vw" }}>
+        <div className="BackgroundTexture"></div>
+        <div className="ForegroundElements">
+          <MainMenu></MainMenu>
+          <GameContext.Provider value={new GameStore()}>
+            <GameView></GameView>
+          </GameContext.Provider>
+        </div>
+      </div>
+    </div>
+  );
 });
 
-export default App
+export default App;
--- src/components/menu_button.tsx
+++ src/components/menu_button.tsx
@@ -1,75 +1,33 @@
-import {css} from "@emotion/react"
+import { css } from "@emotion/react";
 
-interface MenuButtonProps
-{
-	text: string;
-	onClick: () => void;
+interface MenuButtonProps {
+  text: string;
+  onClick: () => void;
 }
 
 const styleMenuButton = css`
-	cursor: pointer;
-	border: 4px solid black;
-	border-radius: 28px;
-	width: 230px;
-	height: 90px;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	font-size: 40px;
-	background-color: #D9D1A9;
-	color: #0D0D0D;
-	clip-path: polygon(
-			0px 20px,
-			4px 20px,
-			4px 12px,
-			8px 12px,
-			8px 8px,
-			12px 8px,
-			12px 4px,
-			16px 4px,
-			20px 4px,
-			20px 0px,
-			calc(100% - 20px) 0px,
-			calc(100% - 20px) 4px,
-			calc(100% - 12px) 4px,
-			calc(100% - 12px) 8px,
-			calc(100% - 8px) 8px,
-			calc(100% - 8px) 12px,
-			calc(100% - 4px) 12px,
-			calc(100% - 4px) 16px,
-			calc(100% - 4px) 20px,
-			100% 20px,
-			100% calc(100% - 20px),
-			calc(100% - 4px) calc(100% - 20px),
-			calc(100% - 4px) calc(100% - 12px),
-			calc(100% - 8px) calc(100% - 12px),
-			calc(100% - 8px) calc(100% - 8px),
-			calc(100% - 12px) calc(100% - 8px),
-			calc(100% - 12px) calc(100% - 4px),
-			calc(100% - 16px) calc(100% - 4px),
-			calc(100% - 20px) calc(100% - 4px),
-			calc(100% - 20px) 100%,
-			20px 100%,
-			20px calc(100% - 4px),
-			12px calc(100% - 4px),
-			12px calc(100% - 8px),
-			8px calc(100% - 8px),
-			8px calc(100% - 12px),
-			4px calc(100% - 12px),
-			4px calc(100% - 16px),
-			4px calc(100% - 20px),
-			0px calc(100% - 20px)
-	);
-	
-	&:active {
-		background-color: #A6A381;
-	}
+  cursor: pointer;
+  border: 4px solid black;
+  border-radius: 28px;
+  width: 230px;
+  height: 90px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
+  background-color: #d9d1a9;
+  color: #0d0d0d;
+
+  &:active {
+    background-color: #a6a381;
+  }
 `;
-const MenuButton = ({text, onClick}: MenuButtonProps) =>
-{
-	return <div css={styleMenuButton} onClick={onClick}>
-		{text}
-	</div>
-}
+const MenuButton = ({ text, onClick }: MenuButtonProps) => {
+  return (
+    <div css={styleMenuButton} onClick={onClick}>
+      {text}
+    </div>
+  );
+};
 
 export default MenuButton;
