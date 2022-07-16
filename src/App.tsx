51f7ed62b6262628d547fc21ee08c753760bc1ef
Yuval Kleiman <yuval.kleiman@me.com>
2022-07-16 12:30:12 +0300

    add background

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,41 @@ import MainMenu from './components/Menus/MainMenu';
 import {AppContext, StoreProps} from "./store";
 import {GameContext, GameStore} from './stores/game_store';
 import GameView from './components/game_view';
+import mainBackground from './assets/main-background.png';
+import { css } from "@emotion/react";
+
 
 const App = observer(() =>
 {
+	const styleGameCanvas = css`
+		height:100vh;
+		width: auto;
+		display: flex;
+		justify-content: center;
+		aspect-ratio: 16 / 9;
+		margin-left: auto;
+		margin-right: auto;
+		image-rendering: pixelated;
+		
+		.BackgroundTexture {
+			background-image: url(${mainBackground});
+			height: 100%;
+			width: auto;
+			aspect-ratio: 16 / 9;
+			margin-left: auto;
+			margin-right: auto;
+			background-repeat: no-repeat;
+			position: absolute;
+			background-size: contain;
+			z-index:0;
+		}
+	`;
+	
 	const store: any = useContext(AppContext);
 	const {count}: any = store;
 
 	return <div className="App">
-		<div className="GameCanvas" style={{height:"100vh",width:"100vw"}}>
+		<div className="GameCanvas" css={styleGameCanvas}>
 			<div className="BackgroundTexture"></div>
 			<div className="ForegroundElements">
 				<MainMenu></MainMenu>
