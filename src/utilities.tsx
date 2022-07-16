5db1dfcccf2cb8e61b0511d9972c0019b04d3ab8
Tomer Norman <spidernor38@gmail.com>
2022-07-16 18:07:03 +0300

    removed some logs and fixed some css with image

--- src/utilities.tsx
+++ src/utilities.tsx
@@ -1,39 +1,30 @@
 const hasOwn = {}.hasOwnProperty;
-export const classNames = function (): string | undefined
-{
-	const classes = [];
+export const classNames = function (): string | undefined {
+  const classes = [];
 
-	for (let i = 0; i < arguments.length; i++) //IE makes problems here with for (...of...) when there are undefined values
-	{
-		let arg = arguments[i];
-		if (!arg)
-			continue;
+  for (
+    let i = 0;
+    i < arguments.length;
+    i++ //IE makes problems here with for (...of...) when there are undefined values
+  ) {
+    let arg = arguments[i];
+    if (!arg) continue;
 
-		let argType = typeof arg;
+    let argType = typeof arg;
 
-		if (argType === 'string' || argType === 'number')
-			classes.push(arg);
+    if (argType === "string" || argType === "number") classes.push(arg);
+    else if (Array.isArray(arg) && arg.length) {
+      let inner = classNames.apply(null, arg as any); //recursive call
+      if (inner) classes.push(inner);
+    } else if (argType === "object") {
+      for (let key in arg) if (hasOwn.call(arg, key) && arg[key]) classes.push(key);
+    }
+  }
 
-		else if (Array.isArray(arg) && arg.length)
-		{
-			let inner = classNames.apply(null, arg as any); //recursive call
-			if (inner)
-				classes.push(inner);
-		}
-		else if (argType === 'object')
-		{
-			for (let key in arg)
-				if (hasOwn.call(arg, key) && arg[key])
-					classes.push(key);
-		}
-	}
-
-	return classes.join(' ') || undefined;
+  return classes.join(" ") || undefined;
 };
 
-export const pick = (arr: any[], amount: number = 1): any[] =>
-{
-	const shuffled = [...arr].sort(() => 0.5 - Math.random());
-	console.log(shuffled);
-	return shuffled.slice(0, amount);
+export const pick = (arr: any[], amount: number = 1): any[] => {
+  const shuffled = [...arr].sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, amount);
 };
