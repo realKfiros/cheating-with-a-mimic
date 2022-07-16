7b20b7f30a513be8401b15e4a06401a0186affb5
Kfir Nevo <nevo.kfir@gmail.com>
2022-07-16 12:19:52 +0300

    dialug

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { useContext, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { observer } from "mobx-react-lite";
-import MainMenu from "./components/menus/MainMenu";
+import MainMenu from "./components/Menus/MainMenu";
 import { AppContext, StoreProps } from "./store";
 import { GameContext, GameStore } from "./stores/game_store";
 import GameView from "./components/game_view";
+import {Dialog} from "./components/dialog";
 
 const App = observer(() => {
   const store: any = useContext(AppContext);
@@ -13,15 +14,16 @@ const App = observer(() => {
 
   return (
     <div className="App">
-      <div className="GameCanvas" style={{ height: "100vh", width: "100vw" }}>
+      <div className="GameCanvas" style={{ height: "100%", width: "100%" }}>
         <div className="BackgroundTexture"></div>
         <div className="ForegroundElements">
-          <MainMenu></MainMenu>
+          <MainMenu />
           <GameContext.Provider value={new GameStore()}>
             <GameView></GameView>
           </GameContext.Provider>
         </div>
       </div>
+		<Dialog />
     </div>
   );
 });
