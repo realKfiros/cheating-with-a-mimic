26996e41d5f0540b3afa01daffb01e2578a94cc2
Kfir Nevo <nevo.kfir@gmail.com>
2022-07-16 12:19:52 +0300

    dialug

--- src/components/Menus/MainMenu.tsx
+++ src/components/Menus/MainMenu.tsx
@@ -1,31 +1,38 @@
-import React from 'react';
-import { css } from "@emotion/react";
+import React, {useContext} from 'react';
+import {css} from "@emotion/react";
 import MenuButton from '../menu_button';
+import {AppContext} from "../../store";
 
 const styleMainMenu = css`
-    display: flex;
-    flex-direction: column;
-    .title {
-        
-    }
-    .button {
+	display: flex;
+	flex-direction: column;
+	color: #0D0D0D;
+	justify-content: center;
+	align-items: center;
 
-    }
-    .start {
+	> * {
+		margin: 25px auto;
+	}
 
-    }
-    .credits {
-
-    }
+	.title {
+		font-size: 64px;
+		margin-bottom: 100px;
+	}
 `;
 
-const MainMenu = () => {
-  return (
-    <div css={styleMainMenu}>
-        <div className="title">Whacky Dice</div>
-        <MenuButton text="start!" onClick={() => window.alert('wow')} />
-    </div>
-  )
+const MainMenu = () =>
+{
+	const {openDialog}: any = useContext(AppContext);
+
+	return <div css={styleMainMenu}>
+		<div className="title">Game Title Here</div>
+		<MenuButton text="Start" onClick={() => window.alert('wow')}/>
+		<MenuButton text="Credits" onClick={() => openDialog('Credits', <ul>
+			<li>Normandeeznuts</li>
+			<li>@realKfiros</li>
+			<li>matos</li>
+		</ul>)}/>
+	</div>;
 }
 
 export default MainMenu;
