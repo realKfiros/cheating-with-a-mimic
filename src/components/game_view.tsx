58fcc120757c2f0aded9d3edfd7d20d9b1ec15f5
yuval kleiman <yuva.kleiman@me.com>
2022-07-17 12:33:36 +0300

    add meters

--- src/components/game_view.tsx
+++ src/components/game_view.tsx
@@ -16,6 +16,7 @@ import dice6 from "../assets/Dice6.png";
 import streetPlayer from "../assets/streetPlayer.png";
 import mainBackground from "../assets/Street-BackGround.png";
 import rollies from "../assets/sounds/dice_rolling.wav";
+import Meter from "../components/meters/Meter";
 
 import BettingAmount from "./modals/BettingAmount";
 import GameWinner from "./modals/GameWinner";
@@ -86,6 +87,15 @@ export const GameView: FC<GameViewProps> = observer(({ showButcher, backgroundLo
       top: 120px;
       left: ${playerLocation}px;
     }
+
+    .hungerBar {
+        left: 320px;
+        top:65px;
+    }
+    .susBar {
+        left: 350px;
+        top:65px;
+    }
   `;
   const [diceAnimStage, setdiceAnimStage] = useState(DiceAnimStage.HIDDEN);
   const [diceFiles, setdiceFiles] = useState(["matos", "matos"]);
@@ -124,6 +134,7 @@ export const GameView: FC<GameViewProps> = observer(({ showButcher, backgroundLo
             ),
             onClose: () => {
                 gameStore.setTableStage(TableStage.WAIT_NEXT_NPC);
+                gameStore.handleGameResult();
             },
           });
       }
@@ -240,7 +251,7 @@ export const GameView: FC<GameViewProps> = observer(({ showButcher, backgroundLo
     }
     if (moveDirection == "right") {
       setTimeout(() => {
-        if (playerLocation < 320) {
+        if (playerLocation < 280) {
           setPlayerLocation(playerLocation + 5);
         } else {
           if (backgroundLocation > -768) {
@@ -344,7 +355,7 @@ export const GameView: FC<GameViewProps> = observer(({ showButcher, backgroundLo
           <div className="userInterface">
             <div className="exitToStreet"></div>
             <div className="moneySum"></div>
-            {/* <div className='hungerBar'></div> */}
+            
             {/* <div className='susMeter'></div> */}
             {/* <div className='cheatButton'></div> */}
           </div>
@@ -362,6 +373,12 @@ export const GameView: FC<GameViewProps> = observer(({ showButcher, backgroundLo
           </div>
         </div>
       )}
+      <div className='spriteObject hungerBar'>
+        <Meter value={gameStore.hunger} label="🍖"/>
+      </div>
+      <div className='spriteObject susBar'>
+        <Meter value={gameStore.suspicion} label="?"/>
+      </div>
     </div>
   );
 });
--- src/components/meters/Meter.tsx
+++ src/components/meters/Meter.tsx
@@ -1,14 +1,42 @@
+import React, { FC } from "react";
 import { css } from "@emotion/react";
 
-const styleMeter = css`
-`
+interface MeterProps {
+    value:number;
+    label:string;
+  }
 
-const Meter = (value:number) => {
-    const barStyle = "height:25px;width:"+value+"%;"
+const Meter:FC<MeterProps> = ({value, label}) => {
+    const style =  css`
+        /* position: absolute;
+        z-index: 3;
+        left: 300px;
+        top:100px; */
+        width:8px;
+        height:102px;
+        background-color: brown;
+        .meterBorder{
+            width: 6px;
+            height: ${value}px;
+            background-color: green;
+            bottom:1px;
+            left:1px;
+            position:absolute;
+        }
+        .meterTitle {
+            text-align:center;
+            position:absolute;
+            top: -20px;
+        }
+    `;
+
     return (
-        <div css={styleMeter}>
+        <div css={style}>
+            <div className="meterTitle">
+                <div>{label}</div>
+            </div>
             <div className="meterBorder">
-                <div className="meterBar" style={barStyle}/>
+                <div className="meterBar" />
             </div>
         </div>
     )
