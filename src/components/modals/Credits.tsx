1d04c25f023ac81a164ed46b00d0d6249c6053c7
Tomer Norman <spidernor38@gmail.com>
2022-07-17 20:24:40 +0300

    improved the in game credits

--- src/components/modals/Credits.tsx
+++ src/components/modals/Credits.tsx
@@ -3,9 +3,12 @@ import React from "react";
 const Credits = () => {
   return (
     <ul>
-      <li>Normandeeznuts</li>
-      <li>@realKfiros</li>
-      <li>matos</li>
+      <li>Tomer Norman - Game design & Programming</li>
+      <li>Kfir Nevo - Programming</li>
+      <li>Yuval Kleiman - Programming</li>
+      <li>Ofri - Programming</li>
+      <li>Aviv Zackham - Art & Graphics</li>
+      <li>Ofek - Art & Graphics</li>
     </ul>
   );
 };
