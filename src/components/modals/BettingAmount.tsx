fd82969b4bce8eef7717e0d3eae538959347e1d6
Tomer Norman <spidernor38@gmail.com>
2022-07-17 18:59:20 +0300

    styled the input

--- src/components/modals/BettingAmount.tsx
+++ src/components/modals/BettingAmount.tsx
@@ -1,8 +1,8 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
 import { css } from "@emotion/react";
 
 interface BettingAmountProps {
-  onChange: (value:number)=>void;
+  onChange: (value: number) => void;
 }
 
 const bettingAmountStyle = css`
@@ -11,14 +11,25 @@ const bettingAmountStyle = css`
   .betting-field {
     color: black;
     background: white;
-    width: 35%;
+    width: 150px;
+    height: 35px;
+    font-size: large;
+    font-family: PixeliodSans;
   }
 `;
 
-const BettingAmount:FC<BettingAmountProps> = ({onChange}) => {
+const BettingAmount: FC<BettingAmountProps> = ({ onChange }) => {
   return (
     <div css={bettingAmountStyle}>
-      <input className="betting-field" name="Bet" type="number" autoFocus onChange={(e)=>{onChange(parseInt(e.target.value))}}/>
+      <input
+        className="betting-field"
+        name="Bet"
+        type="number"
+        autoFocus
+        onChange={(e) => {
+          onChange(parseInt(e.target.value));
+        }}
+      />
     </div>
   );
 };
